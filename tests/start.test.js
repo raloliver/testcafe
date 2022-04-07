4cbce248260ca6934bda11b8438bf7b8b4c02348
raloliver <contato@raloliver.com.br>
2022-04-07 15:16:25 +0100

    Add assertion

--- tests/start.test.js
+++ tests/start.test.js
@@ -2,15 +2,20 @@
  * File: start.test.js
  * Project: testcafe
  * Created: Wednesday, April 6th 2022, 1:34:06 pm
- * Last Modified: Wednesday, April 6th 2022, 1:52:40 pm
+ * Last Modified: Thursday, April 7th 2022, 3:16:11 pm
  * Copyright © 2022 AMDE Agência
  */
+
+import { Selector } from 'testcafe';
 
 fixture`Upping and running with TestCafe`
   .page`https://devexpress.github.io/testcafe/example`;
 
-test('Hello TestCafe', async t => {
+test('should show the name was input on the capture page', async t => {
   // code goes here
   await t.typeText('#developer-name', 'João');
   await t.click('#submit-button');
+
+  // assertion
+  await t.expect(Selector('#article-header').innerText).contains('João');
 });
